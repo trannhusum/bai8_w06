01e05e039db98708a1595905b693a985ae2cb7cc
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-02 19:49:06 +0700

    Initial commit

---
+++ Point2D.cs
@@ -0,0 +1,93 @@
+﻿namespace bai8_w06
+{
+    public class Point2D
+    {
+        private float x;
+        private float y;
+        public Point2D()
+        {
+            x = 0; y = 0;
+        }
+
+        public Point2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public float getX()
+        {
+            return x;
+        }
+        public void setX(float x)
+        {
+            this.x = x;
+        }
+        public float getY()
+        {
+            return y;
+        }
+        public void setY(float y)
+        {
+            this.y = y;
+        }
+        public float[] getXY()
+        {
+            float[] xy = new float[2];
+            xy[0] = x;
+            xy[1] = y;
+            return xy;
+            
+        }
+        public void setXY(float x, float y) 
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return "("+x+","+y+")";
+        }
+    }
+    public class Point3D : Point2D
+    {
+        private float z;
+        public Point3D()
+        {
+            z = 0;
+        }
+
+        public Point3D(float x, float y, float z) : base(x, y)
+        {
+            this.z = z;
+        }
+        public float getZ()
+        {
+            return z;     
+        }
+        public void setZ(float z)
+        {
+            this.z = z;     
+        }
+        public float[] getXYZ()
+        {
+            float[] xyz = new float[3];
+            xyz[0] = getX();
+            xyz[1] = getY();
+            xyz[2] = z;
+            return xyz;
+        }
+        public void setXYZ(float x, float y, float z)
+        {
+            setX(x);
+            setY(y);
+            this.z=z;
+
+        }
+
+        public override string ToString()
+        {
+            return "(" + getX() + "," + getY() +","+z+")";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai8_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D s1 = new Point2D(2, 3);
+            Console.WriteLine(s1);
+            Point3D s2=new Point3D(1,2,3);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+    }
+}
